806ee500f03b2bea4e718a038581695f0b3af70f
Tenfleques <tendaichikae@gmail.com>
2020-06-17 12:21:20 +0300

    plug type bug on frame details

--- gis-eyetracker/ctrls/framedetails.py
+++ gis-eyetracker/ctrls/framedetails.py
@@ -21,9 +21,13 @@ class FrameDetails(GridLayout):
         return get_local_str_util(key)
 
     def update_key(self, label, key, value):
+        if isinstance(value, float):
+            value = datetime.utcfromtimestamp(value).strftime("%H:%M:%S.%f")[:-3]
+        
+        
         id_in_ids = "lbl_{}_{}".format(label, key)
         if id_in_ids in self.ids:
-            self.ids[id_in_ids].text = value
+            self.ids[id_in_ids].text = str(value)
 
     def update(self, **kwargs):
         frame_details = kwargs.get("frame_details", None)
@@ -35,33 +39,24 @@ class FrameDetails(GridLayout):
         self.ids["lbl_camera_fps"].text = "{:.4}".format(kwargs.get("cam_fps", self.ids["lbl_camera_fps"].text))
 
         if frame_details is not None:
-            gaze = frame_details.get("gaze", {"timestamp": ""})
+            gaze = frame_details.get("gaze", {"timestamp": 0})
             if gaze is None:
-                gaze = {"timestamp": ""}
-            video = frame_details.get("video", {"timestamp": ""})
+                gaze = {"timestamp": 0}
+            video = frame_details.get("video", {"timestamp": 0})
             if video is None:
-                video = {"timestamp": ""}
-            camera = frame_details.get("camera", {"timestamp": ""})
+                video = {"timestamp": 0}
+            camera = frame_details.get("camera", {"timestamp": 0})
             if camera is None:
-                camera = {"timestamp": ""}
-            pos = frame_details.get("pos", {"timestamp": ""})
+                camera = {"timestamp": 0}
+            pos = frame_details.get("pos", {"timestamp": 0})
             if pos is None:
-                pos = {"timestamp": ""}
-            origin = frame_details.get("origin", {"timestamp":""})
+                pos = {"timestamp": 0}
+            origin = frame_details.get("origin", {"timestamp":0})
             if origin is None:
-                origin = {"timestamp": ""}
+                origin = {"timestamp": 0}
 
-            ts_tracker = datetime.utcfromtimestamp(gaze.get("timestamp", 0)).strftime("%H:%M:%S.%f")[:-3]
-            self.update_key("tracker_gaze", "time", ts_tracker)
-
-            ts_pos = datetime.utcfromtimestamp(pos.get("timestamp", 0)).strftime("%H:%M:%S.%f")[:-3]
-            self.update_key("tracker_pos", "time", ts_pos)
-
-            ts_origin = datetime.utcfromtimestamp(origin.get("timestamp", 0)).strftime("%H:%M:%S.%f")[:-3]
-            self.update_key("tracker_origin", "time", ts_origin)
-            
-            ts_video = datetime.utcfromtimestamp(video.get("timestamp", 0)).strftime("%H:%M:%S.%f")[:-3]
-            self.update_key("video", "time", ts_video)
-            
-            ts_camera = datetime.utcfromtimestamp(camera.get("timestamp", 0)).strftime("%H:%M:%S.%f")[:-3]
-            self.update_key("camera", "time", ts_camera)
+            self.update_key("tracker_gaze", "time", gaze.get("timestamp", 0))
+            self.update_key("tracker_pos", "time", pos.get("timestamp", 0))
+            self.update_key("tracker_origin", "time", origin.get("timestamp", 0))
+            self.update_key("video", "time", video.get("timestamp", 0))
+            self.update_key("camera", "time", camera.get("timestamp", 0))
